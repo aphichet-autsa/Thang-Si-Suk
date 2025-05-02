686d6d6e6bcc09149d5ef32f0fd65d44899c0ba1
aphichet-autsa <aphichetautsa33@gmail.com>
2025-05-02 18:27:54 +0700

    555

--- frontend/myapp/app/Post.tsx
+++ frontend/myapp/app/Post.tsx
@@ -1,6 +1,7 @@
+// ในไฟล์ PostScreens.js หรือ PostScreens.tsx
 import React from 'react';
-import PostScreen from '../screens/Post';
+import PostScreen from '../screens/Post'; // ตรวจสอบว่าชื่อไฟล์และเส้นทางถูกต้อง
 
 export default function PostScreens() {
-  return <PostScreen/>;
+  return <PostScreen />;
 }
